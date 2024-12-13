1632ac70cbfc77fdd985f006956d49d90abbfcc4
Priyansh Yadav <168275077+priyansh-6307@users.noreply.github.com>
2024-12-13 20:04:32 +0530

    Update App.jsx

--- frontend/bookstore/src/App.jsx
+++ frontend/bookstore/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/footer/footer";
 import './index.css';
